b08b5c6a2db04acb3aeef8771b74fd30559fb6d2
Richard Niemand <niemand.richard@gmail.com>
2020-12-01 22:26:02 -0700

    Moving abstractions

---
+++ src/Rn.NetCore.Common/Abstractions/EnvironmentAbstraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rn.NetCore.Common.Abstractions
+{
+  public interface IEnvironmentAbstraction
+  {
+    string MachineName { get; }
+    string NewLine { get; }
+    string TempFileName(string extension = "tmp");
+  }
+
+  public class EnvironmentAbstraction : IEnvironmentAbstraction
+  {
+    public string MachineName => Environment.MachineName;
+    public string NewLine => Environment.NewLine;
+
+    [Obsolete("NO NOT USE THIS")]
+    public string TempFileName(string extension = "tmp")
+    {
+      // TODO: [TESTS] (EnvironmentAbstraction.TempFileName) Add tests
+      return $"{DateTime.Now.Ticks}.{extension}";
+    }
+  }
+}
---
+++ src/Rn.NetCore.Common/Abstractions/PathAbstraction.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Rn.NetCore.Common.Abstractions
+{
+  public interface IPathAbstraction
+  {
+    string GetExtension(string path);
+    string GetDirectoryName(string path);
+  }
+
+  public class PathAbstraction : IPathAbstraction
+  {
+    public string GetExtension(string path)
+      => Path.GetExtension(path);
+
+    public string GetDirectoryName(string path)
+      => Path.GetDirectoryName(path);
+  }
+}
