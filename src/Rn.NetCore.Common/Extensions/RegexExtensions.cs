378d32529553b76589e98e22b1089005d3073844
Richard Niemand <niemand.richard@gmail.com>
2020-12-01 22:35:41 -0700

    Moving extensions

---
+++ src/Rn.NetCore.Common/Extensions/RegexExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Rn.NetCore.Common.Extensions
+{
+  public static class RegexExtensions
+  {
+    public static bool MatchesRegex(this string input, string pattern)
+    {
+      // TODO: [TESTS] (RegexExtensions.MatchesRegex) Add tests
+
+      if (string.IsNullOrWhiteSpace(input))
+        return false;
+
+      return Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+    }
+
+    public static Match GetRegexMatch(this string input, string pattern)
+    {
+      // TODO: [TESTS] (RegexExtensions.GetRegexMatch) Add tests
+
+      return Regex.Match(input, pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+    }
+  }
+}
---
+++ src/Rn.NetCore.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,71 @@
+﻿namespace Rn.NetCore.Common.Extensions
+{
+  public static class StringExtensions
+  {
+    public static string AppendIfMissing(this string input, string append)
+    {
+      // TODO: [TESTS] (StringExtensions.AppendIfMissing) Add tests
+
+      if (string.IsNullOrWhiteSpace(input) || string.IsNullOrWhiteSpace(append))
+        return input;
+
+      if (input.EndsWith(append))
+        return append;
+
+      return input + append;
+    }
+
+    public static string LowerTrim(this string input)
+    {
+      // TODO: [TESTS] (StringExtensions.LowerTrim) Add tests
+
+      if (string.IsNullOrWhiteSpace(input))
+        return input;
+
+      return input.ToLower().Trim();
+    }
+
+    public static bool AsBool(this string input, bool fallback = false)
+    {
+      // TODO: [TESTS] (StringExtensions.AsBool) Add tests
+
+      if (string.IsNullOrWhiteSpace(input))
+        return fallback;
+
+      switch (input.LowerTrim())
+      {
+        case "true":
+        case "1":
+        case "yes":
+        case "on":
+        case "enabled":
+          return true;
+
+        case "false":
+        case "0":
+        case "no":
+        case "off":
+        case "disabled":
+          return false;
+
+        default:
+          return fallback;
+      }
+    }
+
+    public static int AsInt(this string input, int fallback = 0)
+    {
+      // TODO: [TESTS] (StringExtensions.AsInt) Add tests
+
+      if (string.IsNullOrWhiteSpace(input))
+        return fallback;
+
+      if (int.TryParse(input, out var parsed))
+      {
+        return parsed;
+      }
+
+      return fallback;
+    }
+  }
+}
