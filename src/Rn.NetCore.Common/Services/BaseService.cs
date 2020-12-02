0042f9ab9af0d672dac944b716d227479751ab0a
Richard Niemand <niemand.richard@gmail.com>
2020-12-01 23:23:29 -0700

    Moving base service

---
+++ src/Rn.NetCore.Common/Services/BaseService.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Logging;
+using Rn.NetCore.Common.Metrics;
+
+namespace Rn.NetCore.Common.Services
+{
+  public abstract class BaseService<TService>
+  {
+    public ILogger<TService> Logger { get; }
+    public IMetricService MetricService { get; }
+    public string ServiceName { get; }
+
+
+    // Constructor
+    protected BaseService(
+      ILogger<TService> logger,
+      IMetricService metricService,
+      string serviceName)
+    {
+      Logger = logger;
+      MetricService = metricService;
+      ServiceName = serviceName;
+    }
+
+
+    // Public methods
+    //public async Task<T> TimeServiceCall<T>(string methodName, Func<Task<T>> predicate)
+    //{
+    //  // TODO: [TESTS] (BaseService) Add tests
+
+    //  Logger.LogTrace(
+    //    "Running: {service}.{method}()",
+    //    ServiceName,
+    //    methodName
+    //  );
+
+    //  using (var metric = CreateTimingMetric(methodName))
+    //  {
+    //    try
+    //    {
+    //      var response = await predicate.Invoke();
+    //      metric.SetTag("success", "true");
+    //      return response;
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //      Logger.LogError(ex,
+    //        "Error running service call. {trace}",
+    //        ex.HumanStackTrace()
+    //      );
+
+    //      metric.SetTag("exception", ex.GetType().Name, false);
+    //      return default;
+    //    }
+    //  }
+    //}
+
+    //public T TimeServiceCall<T>(string methodName, Func<T> predicate)
+    //{
+    //  // TODO: [TESTS] (BaseService) Add tests
+
+    //  Logger.LogTrace(
+    //    "Running: {service}.{method}()",
+    //    ServiceName,
+    //    methodName
+    //  );
+
+    //  using (var metric = CreateTimingMetric(methodName))
+    //  {
+    //    try
+    //    {
+    //      var response = predicate.Invoke();
+    //      metric.SetTag("success", "true");
+    //      return response;
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //      Logger.LogError(ex,
+    //        "Error running service call. {trace}",
+    //        ex.HumanStackTrace()
+    //      );
+
+    //      metric.SetTag("exception", ex.GetType().Name, false);
+
+    //      // TODO: [REVISE] (BaseService) Revise impact of this change
+    //      throw;
+    //    }
+    //  }
+    //}
+
+
+    // Private methods
+    //private ICodeTimer CreateTimingMetric(string methodName)
+    //{
+    //  // TODO: [TESTS] (BaseService) Add tests
+    //  // TODO: [LOGGING] (BaseService) Add logging
+
+    //  // Metrics are not enabled, return "NullCodeTimer"
+    //  if (!MetricService.MetricsEnabled)
+    //    return new NullCodeTimer();
+
+    //  // Generate expected tags
+    //  var tags = new Dictionary<string, string>
+    //  {
+    //    ["service"] = ServiceName.Trim(),
+    //    ["method"] = methodName.Trim(),
+    //    ["exception"] = "",
+    //    ["success"] = "false"
+    //  };
+
+    //  // Generate expected fields
+    //  var fields = new Dictionary<string, object>
+    //  {
+    //    ["value"] = 0,
+    //    ["call_count"] = 1
+    //  };
+
+    //  // Return the generated CodeTimer
+    //  return MetricService.LogTiming("service_call", tags, fields);
+    //}
+  }
+}
