13d718fa93df4f83fe667b6ca8164ee910035aa3
Richard Niemand <niemand.richard@gmail.com>
2020-12-01 22:05:34 -0700

    Adding DateTimeAbstraction

---
+++ src/Rn.NetCore.Common/Abstractions/DateTimeAbstraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rn.NetCore.Common.Abstractions
+{
+  public interface IDateTimeAbstraction
+  {
+    DateTime Now { get; }
+    DateTime UtcNow { get; }
+  }
+
+  public class DateTimeAbstraction : IDateTimeAbstraction
+  {
+    public DateTime Now => DateTime.Now;
+    public DateTime UtcNow => DateTime.UtcNow;
+  }
+}
