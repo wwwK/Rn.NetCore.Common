6339bfea4f6342585da28a9777e1684401d254cb
Richard Niemand <niemand.richard@gmail.com>
2020-12-03 21:11:48 -0700

    Working on LineProtocolPoint

---
+++ src/Rn.NetCore.Common/Metrics/Builders/MetricLineBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Rn.NetCore.Common.Metrics.Models;
+
+namespace Rn.NetCore.Common.Metrics.Builders
+{
+  public class MetricLineBuilder
+  {
+    public string Measurement { get; }
+    public Dictionary<string, string> Tags { get; }
+    public Dictionary<string, object> Fields { get; }
+    public DateTime? UtcTimestamp { get; set; }
+
+    // Constructors
+    public MetricLineBuilder(string measurement)
+    {
+      // TODO: [TESTS] (MetricLineBuilder.MetricLineBuilder) Add tests
+      Measurement = measurement;
+      Tags = new Dictionary<string, string>();
+      Fields = new Dictionary<string, object>();
+      UtcTimestamp = null;
+    }
+
+    // Build()
+    public LineProtocolPoint Build(DateTime? utcTimestamp = null)
+    {
+      // TODO: [TESTS] (MetricLineBuilder.Build) Add tests
+      if (utcTimestamp.HasValue)
+        UtcTimestamp = utcTimestamp.Value;
+
+      if (!UtcTimestamp.HasValue)
+        throw new Exception("UtcTimestamp is required!");
+
+      return new LineProtocolPoint(Measurement, Fields, Tags, UtcTimestamp);
+    }
+  }
+}
---
+++ src/Rn.NetCore.Common/Metrics/Models/LineProtocolPoint.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Rn.NetCore.Common.Metrics.Models
+{
+  // FROM: https://github.com/influxdata/influxdb-csharp
+  public class LineProtocolPoint
+  {
+    public string Measurement { get; }
+    public IReadOnlyDictionary<string, object> Fields { get; }
+    public IReadOnlyDictionary<string, string> Tags { get; }
+    public DateTime? UtcTimestamp { get; }
+
+    public LineProtocolPoint(
+      string measurement,
+      IReadOnlyDictionary<string, object> fields,
+      IReadOnlyDictionary<string, string> tags = null,
+      DateTime? utcTimestamp = null)
+    {
+      // RunTick validation on provided values
+      ValidateMeasurement(measurement);
+      ValidateFields(fields);
+      ValidateTags(tags);
+      ValidateTimeStamp(utcTimestamp);
+
+      // Assign values
+      Measurement = measurement;
+      Fields = fields;
+      Tags = tags;
+      UtcTimestamp = utcTimestamp;
+    }
+
+    public void Format(TextWriter textWriter)
+    {
+      if (textWriter == null) throw new ArgumentNullException(nameof(textWriter));
+
+      textWriter.Write(LineProtocolSyntax.EscapeName(Measurement));
+
+      if (Tags != null)
+      {
+        foreach (var t in Tags.OrderBy(t => t.Key))
+        {
+          if (string.IsNullOrEmpty(t.Value))
+            continue;
+
+          textWriter.Write(',');
+          textWriter.Write(LineProtocolSyntax.EscapeName(t.Key));
+          textWriter.Write('=');
+          textWriter.Write(LineProtocolSyntax.EscapeName(t.Value));
+        }
+      }
+
+      var fieldDelim = ' ';
+      foreach (var f in Fields)
+      {
+        textWriter.Write(fieldDelim);
+        fieldDelim = ',';
+        textWriter.Write(LineProtocolSyntax.EscapeName(f.Key));
+        textWriter.Write('=');
+        textWriter.Write(LineProtocolSyntax.FormatValue(f.Value));
+      }
+
+      if (UtcTimestamp != null)
+      {
+        textWriter.Write(' ');
+        textWriter.Write(LineProtocolSyntax.FormatTimestamp(UtcTimestamp.Value));
+      }
+    }
+
+
+    // Validation Methods
+    private static void ValidateMeasurement(string measurement)
+    {
+      if (string.IsNullOrEmpty(measurement))
+      {
+        throw new ArgumentException("A measurement name must be specified");
+      }
+    }
+
+    private static void ValidateFields(IReadOnlyDictionary<string, object> fields)
+    {
+      if (fields == null || fields.Count == 0)
+      {
+        throw new ArgumentException("At least one field must be specified");
+      }
+
+      // ReSharper disable once LoopCanBeConvertedToQuery
+      foreach (var f in fields)
+      {
+        if (string.IsNullOrEmpty(f.Key))
+        {
+          throw new ArgumentException("Fields must have non-empty names");
+        }
+      }
+    }
+
+    private static void ValidateTags(IReadOnlyDictionary<string, string> tags)
+    {
+      if (tags == null)
+        return;
+
+      // ReSharper disable once LoopCanBeConvertedToQuery
+      foreach (var t in tags)
+      {
+        if (string.IsNullOrEmpty(t.Key))
+        {
+          throw new ArgumentException("Tags must have non-empty names");
+        }
+      }
+    }
+
+    private static void ValidateTimeStamp(DateTime? utcTimestamp)
+    {
+      if (utcTimestamp != null && utcTimestamp.Value.Kind != DateTimeKind.Utc)
+      {
+        throw new ArgumentException("Timestamps must be specified as UTC");
+      }
+    }
+  }
+}
