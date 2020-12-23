536c49a77e16da9aa16c71181a719cc274283e3d
Richard Niemand <niemand.richard@gmail.com>
2020-12-22 19:45:53 -0700

    Abstracting Random

---
+++ src/Rn.NetCore.Common/Abstractions/RandomAbstraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rn.NetCore.Common.Abstractions
+{
+  public interface IRandomAbstraction
+  {
+    int Next(int minValue, int maxValue);
+  }
+
+  public class RandomAbstraction : IRandomAbstraction
+  {
+    private readonly Random _random;
+
+    public RandomAbstraction()
+    {
+      _random = new Random(DateTime.Now.Millisecond);
+    }
+
+    public int Next(int minValue, int maxValue)
+    {
+      return _random.Next(maxValue, maxValue);
+    }
+  }
+}
