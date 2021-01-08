e241c9cc6bf43681e0ad99804615e60826c64a00
Richard Niemand <richard.niemand@derivco.co.za>
2021-01-08 14:39:19 -0700

    Adding logging

--- src/DevConsole/Program.cs
+++ src/DevConsole/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Rn.NetCore.Common.Abstractions;
+using Rn.NetCore.Common.Encryption;
+using Rn.NetCore.Common.Helpers;
+using Rn.NetCore.Common.Logging;
+using Rn.NetCore.Common.Metrics;
 
 namespace DevConsole
 {
   class Program
   {
+    private static IServiceProvider _serviceProvider;
+    private static ILoggerAdapter<Program> _logger;
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      ConfigureDI();
+
+      _logger.Info("Hello World!");
+    }
+
+
+    // DI related methods
+    private static void ConfigureDI()
+    {
+      var services = new ServiceCollection();
+
+      var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .Build();
+
+      ConfigureDI_Configuration(services, config);
+      ConfigureDI_Core(services, config);
+
+      _serviceProvider = services.BuildServiceProvider();
+      _logger = _serviceProvider.GetService<ILoggerAdapter<Program>>();
+    }
+
+    private static void ConfigureDI_Core(IServiceCollection services, IConfiguration config)
+    {
+      services
+        .AddSingleton(config)
+        .AddSingleton(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>))
+        .AddSingleton<IEncryptionService, EncryptionService>()
+        .AddSingleton<IEncryptionUtils, EncryptionUtils>()
+        .AddSingleton<IMetricService, MetricService>()
+        .AddSingleton<IDateTimeAbstraction, DateTimeAbstraction>()
+        .AddSingleton<IJsonHelper, JsonHelper>()
+        .AddSingleton<IEnvironmentAbstraction, EnvironmentAbstraction>()
+        .AddSingleton<IDirectoryAbstraction, DirectoryAbstraction>()
+        .AddSingleton<IFileAbstraction, FileAbstraction>()
+        .AddLogging(loggingBuilder =>
+        {
+          // configure Logging with NLog
+          loggingBuilder.ClearProviders();
+          loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+          loggingBuilder.AddNLog(config);
+        });
+    }
+
+    private static void ConfigureDI_Configuration(IServiceCollection services, IConfiguration config)
+    {
+      // Complete
     }
   }
 }
