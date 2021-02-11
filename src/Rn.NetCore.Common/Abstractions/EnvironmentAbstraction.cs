b5b173e5f33a72c678d0d9c155980d3ec22651c3
Richard Niemand <richard.niemand@derivco.co.za>
2021-02-11 14:58:37 -0700

    Extending Environment abstraction

--- src/Rn.NetCore.Common/Abstractions/EnvironmentAbstraction.cs
+++ src/Rn.NetCore.Common/Abstractions/EnvironmentAbstraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Rn.NetCore.Common.Abstractions
 {
@@ -8,6 +9,12 @@ namespace Rn.NetCore.Common.Abstractions
     string NewLine { get; }
     string CurrentDirectory { get; }
     string AppDomainBaseDirectory { get; }
+    string CommandLine { get; }
+
+    IDictionary GetEnvironmentVariables();
+    IDictionary GetEnvironmentVariables(EnvironmentVariableTarget target);
+    string GetEnvironmentVariable(string variable);
+    string GetEnvironmentVariable(string variable, EnvironmentVariableTarget target);
   }
 
   public class EnvironmentAbstraction : IEnvironmentAbstraction
@@ -23,5 +30,20 @@ namespace Rn.NetCore.Common.Abstractions
     
     public string AppDomainBaseDirectory
       => AppDomain.CurrentDomain.BaseDirectory;
+
+    public string CommandLine
+      => Environment.CommandLine;
+
+    public IDictionary GetEnvironmentVariables()
+      => Environment.GetEnvironmentVariables();
+
+    public IDictionary GetEnvironmentVariables(EnvironmentVariableTarget target)
+      => Environment.GetEnvironmentVariables(target);
+
+    public string GetEnvironmentVariable(string variable)
+      => Environment.GetEnvironmentVariable(variable);
+
+    public string GetEnvironmentVariable(string variable, EnvironmentVariableTarget target)
+      => Environment.GetEnvironmentVariable(variable, target);
   }
 }
