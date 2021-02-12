a3e9598caed900b7fec8b5dfa7f9bb6f61cf36cc
Richard Niemand <richard.niemand@derivco.co.za>
2021-02-12 15:08:43 -0700

    Adding start of HttpClient abstraction

---
+++ src/Rn.NetCore.Common/Factories/HttpClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using Rn.NetCore.Common.Services;
+using Rn.NetCore.Common.Wrappers;
+
+namespace Rn.NetCore.Common.Factories
+{
+  public interface IHttpClientFactory
+  {
+    IHttpClient NewHttpClient();
+    IHttpClient NewHttpClient(TimeoutHandler timeoutHandler);
+  }
+
+  public class HttpClientFactory : IHttpClientFactory
+  {
+    public IHttpClient NewHttpClient()
+    {
+      // TODO: [TESTS] (HttpClientFactory.NewHttpClient) Add tests
+      return new HttpClientWrapper();
+    }
+
+    public IHttpClient NewHttpClient(TimeoutHandler timeoutHandler)
+    {
+      // TODO: [TESTS] (HttpClientFactory.NewHttpClient) Add tests
+      return new HttpClientWrapper(timeoutHandler);
+    }
+  }
+}
