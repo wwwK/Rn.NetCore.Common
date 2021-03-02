4fbcc40ae58b1d81c4ecb020e0de7a19b6e0b094
Richard Niemand <richard.niemand@derivco.co.za>
2021-03-02 14:21:55 -0700

    Adding RandomHelper

---
+++ src/Rn.NetCore.Common/Helpers/RandomHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Rn.NetCore.Common.Helpers
+{
+  public interface IRandomHelper
+  {
+    string RandomString(int length);
+    string RandomString(int length, string seed);
+  }
+
+  public class RandomHelper : IRandomHelper
+  {
+    public const string AlphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public const string Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public const string Numeric = "0123456789";
+
+    private readonly Random _random;
+
+    public RandomHelper()
+    {
+      // TODO: [TESTS] (RandomHelper.RandomHelper) Add tests
+      _random = new Random(DateTime.Now.Millisecond);
+    }
+
+    public string RandomString(int length)
+      => RandomString(length, AlphaNumeric);
+
+    public string RandomString(int length, string seed)
+    {
+      // TODO: [TESTS] (RandomHelper.RandomString) Add tests
+      return new string(Enumerable
+        .Repeat(seed, length)
+        .Select(s => s[_random.Next(s.Length)])
+        .ToArray()
+      );
+    }
+  }
+}
