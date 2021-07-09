154ff78054c91ac28155eefcdbffa38590cd9974
Richard Niemand <richard.niemand@derivco.co.za>
2021-07-09 17:56:33 -0600

    Daily

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,23 +1,29 @@
-$PublishDirectoryName  = "artifacts";
-$Configuration         = "Release";
+$workingDir         = $PSScriptRoot;
+$sourceDir          = Join-Path $workingDir "src";
+$artifactDir        = Join-Path $workingDir "artifacts";
+$publishDir         = Join-Path $artifactDir "publish";
+$slnCommon          = Join-Path $sourceDir "Rn.NetCore.Common";
+$slnWebCommon       = Join-Path $sourceDir "Rn.NetCore.WebCommon";
+$slnMetricsRabbit   = Join-Path $sourceDir "Rn.NetCore.Metrics.Rabbit";
+$buildConfiguration = "Release";
+$buildCmd           = "";
+$curPublishDir      = "";
 
-# Setup directories
-$workingDir       = $PSScriptRoot;
-$sourceDir        = Join-Path $workingDir "src";
-$slnCommon        = Join-Path $sourceDir "Rn.NetCore.Common";
-$slnWebCommon     = Join-Path $sourceDir "Rn.NetCore.WebCommon";
-$slnMetricsRabbit = Join-Path $sourceDir "Rn.NetCore.Metrics.Rabbit";
-$buildCmd         = ""
-
+# =============================================================================
 # Build projects
-$buildCmd = "dotnet build $slnCommon --configuration $Configuration"
-Write-Host "Running Build: $buildCmd"
+# =============================================================================
+#
+$curPublishDir    = Join-Path $publishDir "Rn.NetCore.Common\";
+$buildCmd         = "dotnet build $slnCommon --configuration $buildConfiguration --output `"$curPublishDir`""
+Write-Host        "Running Build: $buildCmd"
 Invoke-Expression $buildCmd;
 
-$buildCmd = "dotnet build $slnWebCommon --configuration $Configuration" 
-Write-Host "Running Build: $buildCmd"
+$curPublishDir    = Join-Path $publishDir "Rn.NetCore.WebCommon\";
+$buildCmd         = "dotnet build $slnWebCommon --configuration $buildConfiguration --output `"$curPublishDir`""
+Write-Host        "Running Build: $buildCmd"
 Invoke-Expression $buildCmd;
 
-$buildCmd = "dotnet build $slnMetricsRabbit --configuration $Configuration" 
-Write-Host "Running Build: $buildCmd"
+$curPublishDir    = Join-Path $publishDir "Rn.NetCore.Metrics.Rabbit\";
+$buildCmd         = "dotnet build $slnMetricsRabbit --configuration $buildConfiguration --output `"$curPublishDir`""
+Write-Host        "Running Build: $buildCmd"
 Invoke-Expression $buildCmd;
